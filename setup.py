56cca60b312e9a21ab1b31b1bb5c392d7acc54fd
karthiga36 <karthigaramamoorthy36@gmail.com>
2024-11-11 02:10:15 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='my_pip_package',
+    version='dev',
+
+    url='https://github.com/karthiga36/dsss_homework_2.git',
+    author='Karthiga36',
+    author_email='karthigaramamoorthy36@gmail.com',
+
+    #py_modules=['my_pip_package'],
+)
